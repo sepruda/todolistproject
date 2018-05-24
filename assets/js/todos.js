fb1cbc0fa89cb180fdc465483d80e7a4742275c9
Christian Jess Mark <christianjessmark@gmail.com>
2018-05-24 22:53:58 +0200

    Initial commit

---
+++ assets/js/todos.js
@@ -0,0 +1,22 @@
+// Check off specific Todos by clicking
+$("ul").on("click", "li", function(){
+	$(this).toggleClass("completed");
+});
+
+//Delete todos with click on X
+$("ul").on("click", "span", function(event){
+	$(this).parent().fadeOut(500, function(){
+		$(this).remove();
+	});
+	event.stopPropagation();
+});
+
+$("input[type='text'").on("keypress", function(event){
+	if (event.which === 13) {
+		//grabbing new todo text from input
+		let todoText = $(this).val()
+		$(this).val("");
+		//creat a new li add to ul
+		$("ul").append("<li><span>X </span>" + todoText + "</li>");
+	}
+})
